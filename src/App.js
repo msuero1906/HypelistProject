4aa4fa44f6adc93e24e38d74fbc1b9a310408c99
msuero1906 <43377325+msuero1906@users.noreply.github.com>
2021-11-10 17:53:38 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import "./styles.css";
+//Write array below
+export default function App() {
+  return (
+    <div className="App">
+      <table>
+        <th>First Name</th>
+        <tr>
+          <td>Name</td>
+        </tr>
+        <tr>
+          <td>Name</td>
+        </tr>
+        <tr>
+          <td>Name</td>
+        </tr>
+        <tr>
+          <td>Name</td>
+        </tr>
+        <tr>
+          <td>Name</td>
+        </tr>
+      </table>
+    </div>
+  );
+}
