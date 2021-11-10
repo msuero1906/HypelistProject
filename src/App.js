2c76fafa304140e36591348c81c8ca68cb3917af
msuero1906 <43377325+msuero1906@users.noreply.github.com>
2021-11-10 18:29:44 -0500

    map function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import "./styles.css";
 //Write array below
 export default function App() {
+  let names = ["Omari","Melody","Keturah","Aldo","Reginea","Amori","Laurent","Ye","Matt","Judy","Sami","Maxwell","Groana","Aland","Jean","Lorena"];
+
   return (
     <div className="App">
       <table>
         <th>First Name</th>
+        { names.map(function(name){
+          return()
+        })
         <tr>
-          <td>Name</td>
+          <td>{names[0]}</td>
         </tr>
-        <tr>
-          <td>Name</td>
-        </tr>
-        <tr>
-          <td>Name</td>
-        </tr>
-        <tr>
-          <td>Name</td>
-        </tr>
-        <tr>
-          <td>Name</td>
-        </tr>
+        }
+       
       </table>
     </div>
   );
