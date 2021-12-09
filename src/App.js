24852470421c6a369ff858279347bdb209bec86a
msuero1906 <43377325+msuero1906@users.noreply.github.com>
2021-12-08 19:12:06 -0500

    added last names

--- src/App.js
+++ src/App.js
@@ -2,22 +2,22 @@ import "./styles.css";
 //Write array below
 export default function App() {
   let people = [
-    { FirstName: "Omari", LastName: "" },
-    "Melody",
-    "Keturah",
-    "Aldo",
-    "Reginea",
-    "Amori",
-    "Laurent",
-    "Ye",
-    "Matt",
-    "Judy",
-    "Sami",
-    "Maxwell",
-    "Groana",
-    "Aland",
-    "Jean",
-    "Lorena"
+    { FirstName: "Omari", LastName: "Prass" },
+    { FirstName: "Melody", LastName: "Suero" },
+    { FirstName: "Keturah", LastName: "Allen" },
+    { FirstName: "Aldo", LastName: "Terrana" },
+    { FirstName: "Reginea", LastName: "Walker" },
+    { FirstName: "Amori", LastName: "Steele" },
+    { FirstName: "Laurent", LastName: "Benjamin" },
+    { FirstName: "Ye", LastName: "Paing" },
+    { FirstName: "Matt", LastName: "Schupack" },
+    { FirstName: "Judy", LastName: "Truong" },
+    { FirstName: "Sami", LastName: "Lieberman" },
+    { FirstName: "Maxwell", LastName: "Williams" },
+    { FirstName: "Groana", LastName: "Melendez" },
+    { FirstName: "Aland", LastName: "Kuang" },
+    { FirstName: "Jean", LastName: "Saldarriaga" },
+    { FirstName: "Lorena", LastName: "Castillo" }
   ];
 
   return (
@@ -25,12 +25,14 @@ export default function App() {
       <table>
         <tr>
           {" "}
-          <th>First Name</th>{" "}
+          <th>First Name</th>
+          <th>Last Name</th>
         </tr>
         {people.map(function (person) {
           return (
             <tr>
               <td>{person.FirstName}</td>
+              <td>{person.LastName}</td>
             </tr>
           );
         })}
